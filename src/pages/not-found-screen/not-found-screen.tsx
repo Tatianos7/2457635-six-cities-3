d86449e8f18bafbdb54de2aa31bab8ea509ec5e5
Tatianos7 <152062638+Tatianos7@users.noreply.github.com>
2026-01-14 15:12:02 +0300

    Правка 2

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,6 +1,5 @@
 import { Link } from 'react-router-dom';
 import './no-found-screen.css';
-// import '../not-found-screen/not-found-screen.css';
 import Image from '../../img/Murlok.png';
 
 export default function NotFoundScreen () {
